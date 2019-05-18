d058c76962cce7945158b6dde1784a654a580383
陈浩chh <haochh.chen@qunar.com>
2019-05-18 20:35:13 +0800

    Initial commit

---
+++ src/main/java/com/haochh/chen/poseidon/bean/Greeting.java
@@ -0,0 +1,7 @@
+package com.haochh.chen.poseidon.bean;
+
+/**
+ * Created by admin on 2019/5/18.
+ */
+public class Greeting {
+}
---
+++ src/main/java/com/haochh/chen/poseidon/handler/GreetingHandler.java
@@ -0,0 +1,7 @@
+package com.haochh.chen.poseidon.handler;
+
+/**
+ * Created by admin on 2019/5/18.
+ */
+public class GreetingHandler {
+}
